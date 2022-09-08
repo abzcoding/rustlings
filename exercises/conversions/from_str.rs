61d7cf27c6b3149a4bf9263f9bb9558c0229265b
Abouzar Parvan <abz@sotoon.ir>
2022-09-08 15:26:45 +0430

    I am Done

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -28,8 +28,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -46,6 +44,27 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+        let parts: Vec<&str> = s.split(',').collect();
+        if parts.len() == 2 {
+            if parts[0].len() > 0 {
+                let age: Result<usize, _> = parts[1].parse();
+                if let Ok(age) = age {
+                    Ok(Person {
+                        name: parts[0].into(),
+                        age: age.into(),
+                    })
+                } else {
+                    Err(ParsePersonError::ParseInt(age.unwrap_err()))
+                }
+            } else {
+                Err(ParsePersonError::NoName)
+            }
+        } else {
+            Err(ParsePersonError::BadLen)
+        }
     }
 }
 
--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -14,18 +14,34 @@
 
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+use std::fmt::Formatter;
 
 pub struct ReportCard {
-    pub grade: f32,
+    pub grade: Grade,
     pub student_name: String,
     pub student_age: u8,
 }
 
+pub enum Grade {
+    Numeric(f32),
+    Alpha(String),
+}
+
+impl std::fmt::Display for Grade {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Grade::Numeric(n) => write!(f, "{}", n),
+            Grade::Alpha(a) => write!(f, "{}", a),
+        }
+    }
+}
+
 impl ReportCard {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade
+        )
     }
 }
 
@@ -36,7 +52,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Numeric(2.1),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -48,9 +64,8 @@ mod tests {
 
     #[test]
     fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Alpha("A+".to_string()),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
